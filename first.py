5c8e14444b6055b925da550142993984fbea3a8a
lalithya <lalithya.s10@gmail.com>
2025-09-24 16:53:52 -0400

    updated py code

--- first.py
+++ first.py
@@ -2,9 +2,13 @@
 
 a= "lalithya"
 b= 27
-c="python developer"
-print(a)
-print(b)
-print(c)
+c= 5.6
+d="python developer"
 
-print ("My name is", a,"my age is",b,"and i am a", c)
+#print(a)
+#print(b)
+#print(c)
+
+#print ("My name is", a,"my age is",b,"and i am a", c)
+
+print(f"my name is {a},my age is {b}, my height is {c} and i am a {d}")
